75880989bb7a5acb068fe382a8b6fa68fb6f58cc
therealminhduc <41884926+therealminhduc@users.noreply.github.com>
2022-02-08 14:47:58 +0100

    Add files via upload

---
+++ Intent/Intent.java
@@ -0,0 +1,23 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void LanceUvsq(View view){
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.uvsq.fr/"));
+            startActivity(intent);
+    }
+}
