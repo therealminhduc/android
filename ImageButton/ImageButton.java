ee7b1188b4e3367fa09635245ad7bd8ce11f274f
therealminhduc <41884926+therealminhduc@users.noreply.github.com>
2022-02-08 14:35:29 +0100

    Add files via upload

---
+++ ImageButton/ImageButton.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton imageButton;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView = findViewById(R.id.textView);
+                textView.setText(R.string.bonjour);
+            }
+        });
+
+    }
+}
