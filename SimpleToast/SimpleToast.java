354cbe7bc0943908943e73098a4aba4ca93672ae
therealminhduc <41884926+therealminhduc@users.noreply.github.com>
2022-02-08 15:06:56 +0100

    Add files via upload

---
+++ SimpleToast/SimpleToast.java
@@ -0,0 +1,29 @@
+package com.example.simpletoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SimpleToast extends AppCompatActivity {
+    Button button;
+    Toast toast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Toast.makeText(SimpleToast.this, "Toast", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
